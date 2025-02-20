be6feb49493589b14a9ccb12ba638f562af4d3f3
Alexander3356 <alexander.mascaro@samtrevano.ch>
2025-02-20 21:47:14 +0100

    esercizio

--- 4.9/Confronto/src/Confronto.java
+++ 4.9/Confronto/src/Confronto.java
@@ -1,15 +1,32 @@
 
+/**
+ *Esercizio 4.9
+ *
+ * @author Alexander Mascaro
+ * @version 20.02.2025
+ */
 
 public class Confronto {
     public static void main(String[] args) {
 
-        String[] ordinati = new String[args.length];
 
-        while (ordinati.length < args.length) {
-            for (String parola : args) {
-                int lunghezza = parola.length();
-                
+        for (int i = 0; i < args.length - 1; i++) {
+
+            for (int j = 0; j < args.length - 1 ; j++) {
+
+                if (args[j].length() > args[j + 1].length()) {
+                    String parola = args[j];
+                    args[j] = args[j + 1];
+                    args[j + 1] = parola;
+                }
+
             }
         }
+
+        System.out.println("Array: ");
+        for (String elemento : args) {
+            System.out.println(elemento);
+        }
+
     }
 }
